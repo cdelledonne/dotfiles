fac083266547303d789bc2dd8809b663a528eba7
cdelledonne <carlo.delle.donne@gmail.com>
2019-04-26 00:50:00 +0200

    New install script started

---
+++ install.sh
@@ -0,0 +1,6 @@
+VIM_CONFIG_DIR="$HOME/.vim/"
+NVIM_CONFIG_DIR="$HOME/.config/nvim/"
+
+echo "Use empty answers for default values."
+echo "Vim's configuration directory [ $VIM_CONFIG_DIR ]: "
+echo "Neovim's configuration directory [ $NVIM_CONFIG_DIR ]: "
