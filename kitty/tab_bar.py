6899d91b819835b564acc67661f97104664c991e
Carlo Delle Donne <carlo.delle.donne@gmail.com>
2023-04-15 11:53:31 +0200

    Refine tab_bar.py to support pager icon

--- kitty/tab_bar.py
+++ kitty/tab_bar.py
@@ -2,14 +2,27 @@ import getpass
 import math
 import socket
 from pathlib import Path
-from typing import Any, Dict, Optional
-
-from paramiko import SSHConfig
+from typing import Any, Callable, Dict, List, Optional
 
 from kittens.ssh.main import get_connection_data
-from kitty.fast_data_types import Color, Screen, get_boss
-from kitty.tab_bar import Dict, DrawData, ExtraData, TabBarData, as_rgb, draw_title
+from paramiko import SSHConfig
+
+from kitty.boss import Boss
+from kitty.fast_data_types import (
+    Color,
+    Screen,
+    get_boss,
+)
+from kitty.tab_bar import (
+    Dict,
+    DrawData,
+    ExtraData,
+    TabBarData,
+    as_rgb,
+    draw_title,
+)
 from kitty.utils import color_as_int
+from kitty.window import Window
 
 # Path to SSH config file (should be standard), needed to lookup host names and retrieve
 # corresponding user names
@@ -32,6 +45,7 @@ PADDING = " "
 BRANCH_ICON = "󰘬"
 USER_ICON = ""
 HOST_ICON = "󱡶"
+PAGER_ICON = "󰦪"
 
 # Colors
 SOFT_SEP_COLOR = Color(89, 89, 89)
@@ -40,15 +54,32 @@ ACCENTED_BG_COLOR = Color(30, 104, 199)
 ACCENTED_ICON_BG_COLOR = Color(53, 132, 228)
 
 MIN_TAB_LEN = (
-    len(LEFT_SEP) + len(RIGHT_SEP) +  # separators
-    len(PADDING) * 2 * PADDED_TABS +  # padding
-    1 + 1 + 1 + 1 +                   # tab symbol, space, index number, space
-    1                                 # one title character
+    len(LEFT_SEP)
+    + len(RIGHT_SEP)
+    + len(PADDING) * 2 * PADDED_TABS  # separators
+    + 1  # padding
+    + 1
+    + 1
+    + 1
+    + 1  # tab symbol, space, index number, space  # one title character
 )
 
 
+class Button:
+    def __init__(self, first_cell: int, last_cell: int, action: Callable):
+        self.first_cell = first_cell
+        self.last_cell = last_cell
+        self.action = action
+
+    def do_action(self) -> None:
+        self.action()
+
+
+buttons: List[Button] = []
+
+
 def _draw_element(
-    element: DrawData | str,
+    title: DrawData | str,
     screen: Screen,
     tab: TabBarData,
     before: int,
@@ -60,13 +91,13 @@ def _draw_element(
     accented: bool = False,
     icon: Optional[str] = None,
     soft_sep: Optional[str] = None,
-) -> None:
+) -> int:
     # When max_tab_length < MIN_TAB_LEN, we just draw an ellipsis, without separators or
     # anything else, so that it's clear that one either needs a larger max_tab_length,
     # or another tab bar design
     if max_tab_length < MIN_TAB_LEN:
         screen.draw("…".center(max_tab_length))
-        return
+        return screen.cursor.x
 
     if accented:
         text_fg = colors["accented_fg"]
@@ -81,55 +112,96 @@ def _draw_element(
         text_bg = colors["bg"]
         icon_bg = colors["bg"]
 
-    # Left separator, left padding, icon
-    left_components = list()
-    left_components.append((LEFT_SEP, icon_bg, colors["bg"]))
+    components = list()
+
+    # Left separator
+    components.append((LEFT_SEP, icon_bg, colors["bg"]))
+    # Padding between left separator and rest of tab
     if padded:
-        left_components.append((PADDING, text_fg, text_bg))
+        components.append((PADDING, text_fg, text_bg))
+    # Icon, with padding on the right if there's a tab title, and more padding before
+    # title if the tab is filled and there's a tab title
     if icon:
-        left_components.append((f"{icon}{PADDING}", text_fg, icon_bg))
-        if filled:
-            left_components.append((PADDING, text_fg, text_bg))
-    for c in left_components:
-        screen.cursor.fg = c[1]
-        screen.cursor.bg = c[2]
-        screen.draw(c[0])
-
+        icon_padding = PADDING if title != "" else ""
+        components.append((f"{icon}{icon_padding}", text_fg, icon_bg))
+        if filled and title != "":
+            components.append((PADDING, text_fg, text_bg))
     # Title
-    screen.cursor.fg = text_fg
-    screen.cursor.bg = text_bg
-    if isinstance(element, DrawData):
-        draw_title(element, screen, tab, index)
-        max_cursor_x = before + max_tab_length - len(LEFT_SEP) - len(PADDING)
-        if screen.cursor.x > max_cursor_x:
-            screen.cursor.x = max_cursor_x - 1
-            screen.draw("…")
-    else:
-        screen.draw(element)
-
-    # Right padding, right separator, inter-tab soft separator
-    right_components = list()
+    components.append((title, text_fg, text_bg))
+    # Padding between tab content and right separator
     if padded:
-        right_components.append((PADDING, text_fg, text_bg))
-    right_components.append((RIGHT_SEP, text_bg, colors["bg"]))
+        components.append((PADDING, text_fg, text_bg))
+    # Right separator, which is drawn using the same colors as the left separator in
+    # case there isn't a tab title
+    right_sep_fg = text_bg if title != "" else icon_bg
+    components.append((RIGHT_SEP, right_sep_fg, colors["bg"]))
+    # Inter-tab soft separator
     if soft_sep:
-        right_components.append((soft_sep, colors["soft_sep_fg"], colors["bg"]))
-    for c in right_components:
+        components.append((soft_sep, colors["soft_sep_fg"], colors["bg"]))
+
+    for c in components:
         screen.cursor.fg = c[1]
         screen.cursor.bg = c[2]
-        screen.draw(c[0])
+        if isinstance(c[0], str):
+            screen.draw(c[0])
+        else:
+            draw_title(c[0], screen, tab, index)
+            max_cursor_x = before + max_tab_length - len(LEFT_SEP) - len(PADDING)
+            if screen.cursor.x > max_cursor_x:
+                screen.cursor.x = max_cursor_x - 1
+                screen.draw("…")
+
+    # Element ends before soft separator
+    end = screen.cursor.x - (len(soft_sep) if soft_sep else 0)
+    return end
 
 
-def _get_system_info() -> Dict[str, Any]:
-    window = get_boss().active_window
+def _calc_elements_len(elements: List[Dict[str, Any]]) -> int:
+    elements_len = 0
+    for element in elements:
+        title, icon = element["title"], element["icon"]
+        # Icon
+        elements_len += len(icon)
+        # Icon padding, if element has a title
+        elements_len += len(PADDING) if title != "" else 0
+        # Title
+        elements_len += len(title)
+        # Separators
+        elements_len += 2
+        if RHS_STATUS_FILLED:
+            # Title padding, if element has a title
+            elements_len += len(PADDING) if title != "" else 0
+    # Inter-tab soft separators
+    elements_len += len(elements) - 1
+    return elements_len
 
+
+def _is_running_pager(active_window: Window) -> bool:
+    cmdline = active_window.child.foreground_cmdline
+    print(cmdline)
+    if (
+        len(cmdline) >= 2
+        and cmdline[0] == "python"
+        and Path(cmdline[1]).name == "nvim-pager.py"
+    ):
+        return True
+    return False
+
+
+def _get_system_info(active_window: Window) -> Dict[str, Any]:
     # Local info (and fallback for errors on remote info)
     user = getpass.getuser()
     host = socket.gethostname()
     is_ssh = False
 
-    # The following variable is not an empty list in case we're running the ssh kitten
-    ssh_cmdline = window.ssh_kitten_cmdline()
+    ssh_cmdline = []
+    if active_window.child_is_remote:
+        # The propery "child_is_remote" is True when the command being executed is a
+        # standard "ssh" command, without using the SSH kitten
+        ssh_cmdline = active_window.child.foreground_cmdline
+    else:
+        # The command line is not an empty list in case we're running the ssh kitten
+        ssh_cmdline = active_window.ssh_kitten_cmdline()
 
     if ssh_cmdline != []:
         is_ssh = True
@@ -154,10 +226,6 @@ def _get_system_info() -> Dict[str, Any]:
             print("Could not retrieve SSH connection data")
 
     return {"user": user, "host": host, "is_ssh": is_ssh}
-
-
-def _get_git_info() -> Dict[str, Any]:
-    return {"branch": "main"}
 
 
 def draw_tab(
@@ -212,7 +280,7 @@ def draw_tab(
             soft_sep = SOFT_SEP if both_inactive else PADDING
 
     # Draw main tabs
-    _draw_element(
+    end = _draw_element(
         draw_data,
         screen,
         tab,
@@ -225,31 +293,29 @@ def draw_tab(
         soft_sep=soft_sep,
     )
 
-    end = screen.cursor.x
-
     # Draw right-hand side status
     if is_last:
-        system_info = _get_system_info()
-        git_info = _get_git_info()
-        elements = [
-            # (git_info["branch"], BRANCH_ICON, False),
-            (system_info["user"], USER_ICON, system_info["is_ssh"]),
-            (system_info["host"], HOST_ICON, system_info["is_ssh"]),
-        ]
+        boss: Boss = get_boss()
+        active_window = boss.active_window
+        assert isinstance(active_window, Window)
+
+        is_running_pager = _is_running_pager(active_window)
+        sys_info = _get_system_info(active_window)
+        user, host, is_ssh = sys_info["user"], sys_info["host"], sys_info["is_ssh"]
+
+        elements = list()
+        if is_running_pager:
+            elements.append({"title": "", "icon": PAGER_ICON, "accented": True})
+        elements.append({"title": user, "icon": USER_ICON, "accented": is_ssh})
+        elements.append({"title": host, "icon": HOST_ICON, "accented": is_ssh})
+
         # Move cursor horizontally so that right-hand side status is right-aligned
-        rhs_status_len = 0
-        for element in elements:
-            rhs_status_len += len(element[1])  # icon
-            rhs_status_len += len(PADDING)  # icon padding
-            rhs_status_len += len(element[0])  # text
-            rhs_status_len += 2  # separators
-            if RHS_STATUS_FILLED:
-                rhs_status_len += len(PADDING)  # text padding
-        rhs_status_len += len(elements) - 1  # soft separators
+        rhs_status_len = _calc_elements_len(elements)
         screen.cursor.x = math.ceil(screen.columns / 2 + end / 2) - rhs_status_len
+
         for element in elements:
             _draw_element(
-                element[0],
+                element["title"],
                 screen,
                 tab,
                 before,
@@ -258,8 +324,8 @@ def draw_tab(
                 colors,
                 filled=RHS_STATUS_FILLED,
                 padded=False,
-                accented=element[2],
-                icon=element[1],
+                accented=element["accented"],
+                icon=element["icon"],
                 soft_sep=PADDING,
             )
 
