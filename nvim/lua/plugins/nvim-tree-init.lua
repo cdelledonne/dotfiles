621fce3a739f08970b4b6fc40377cf6ad60feda1
Carlo Delle Donne <carlo.delle.donne@gmail.com>
2023-09-26 15:53:19 +0200

    Tweak config of nvim-tree

--- nvim/lua/plugins/nvim-tree-init.lua
+++ nvim/lua/plugins/nvim-tree-init.lua
@@ -1,22 +1,29 @@
 local nvimtree = require('nvim-tree')
 
 nvimtree.setup({
-    -- Disable git integration
-    git = { enable = false },
-    -- Hide hidden files, and hide some system files
+    actions = {
+        open_file = {
+            -- Do not resize window when opening file
+            resize_window = false,
+        },
+    },
     filters = {
+        -- Hide hidden files
         dotfiles = true,
+        -- Hide some system files
         custom = { '.DS_Store', '*.swp' },
     },
-    view = {
-        hide_root_folder = true,
-        signcolumn = 'no',
-    },
+    -- Disable git integration
+    git = { enable = false },
     renderer = {
         icons = {
             symlink_arrow = ' ➜ ',
             glyphs = { default = '', symlink = '' },
         },
+        root_folder_label = false,
         special_files = {},
-    }
+    },
+    view = {
+        signcolumn = 'no',
+    },
 })
