5a2f24f95d75c6c30a3595056a1a77e311b331b7
Carlo Delle Donne <carlo.delle.donne@gmail.com>
2023-09-30 12:07:31 +0200

    Fix config for indent-blankline

--- nvim/lua/plugins/indent-blankline-init.lua
+++ nvim/lua/plugins/indent-blankline-init.lua
@@ -1,11 +1,22 @@
-local indent_blankline = require('indent_blankline')
+local ibl = require('ibl')
+local hooks = require('ibl.hooks')
 
-indent_blankline.setup({
-    max_indent_increase = 1,
-    show_first_indent_level = false,
-    filetype_exclude = { 'help', 'NvimTree' },
-    show_trailing_blankline_indent = false,
+ibl.setup({
+    scope = {
+        enabled = false,
+        show_start = false,
+        show_end = false,
+    },
+    -- max_indent_increase = 1,
+    -- show_first_indent_level = false,
+    -- filetype_exclude = { 'help', 'NvimTree' },
+    -- show_trailing_blankline_indent = false,
     -- show_current_context = true,
     -- use_treesitter = true,
     -- use_treesitter_scope = true,
 })
+
+hooks.register(
+    hooks.type.WHITESPACE,
+    hooks.builtin.hide_first_space_indent_level
+)
