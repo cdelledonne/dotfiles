64e1e55741fc51f38d7f451c39db6b3dbd23958c
Carlo Delle Donne <cdelledonne@mailbox.org>
2024-03-17 22:58:12 +0100

    Add borders for diagnostics

--- nvim/lua/plugins/nvim-lspconfig-init.lua
+++ nvim/lua/plugins/nvim-lspconfig-init.lua
@@ -1,11 +1,27 @@
 -- Customize how diagnostics are displayed
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
-        virtual_text = false,
-        signs = false,
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = false,
+})
+
+-- Customize highlights and borders of floating window
+vim.cmd("highlight! link NormalFloat None")
+vim.cmd("highlight! link FloatBorder None")
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or {
+        {"╭", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╮", "FloatBorder"},
+        {"│", "FloatBorder"},
+        {"╯", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╰", "FloatBorder"},
+        {"│", "FloatBorder"},
     }
-)
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
 
 -- Show line diagnostics automatically in hover window
 -- vim.o.updatetime = 1000
